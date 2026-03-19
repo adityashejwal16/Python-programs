f943cdb1ad16df1c7e6d30632c29bdf56daf79ef
Aditya Shejwal <adityashejwal016@gmail.com>
2026-03-19 17:56:28 +0530

    Add files via upload

---
+++ program934.py
@@ -0,0 +1,15 @@
+No1 = 0
+No2 = 0
+Ans = 0
+
+ # issue 
+ 
+print("Enter First Number : ")
+No1 = input()
+
+print("Enter Secound Number : ")
+No2 = input()
+
+Ans = No1 + No2
+
+print("Addition is : ",Ans)
