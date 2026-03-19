4f3b97eaba4c98b15f010b5e0a58767140f806a2
Aditya Shejwal <adityashejwal016@gmail.com>
2026-03-19 17:58:21 +0530

    Add files via upload

---
+++ program939.py
@@ -0,0 +1,16 @@
+#return max NO
+def main():
+    print("Enter First Number :")
+    No1 = int(input())
+
+    print("Enter Secound Number :")
+    No2 = int(input())
+
+    if(No1 > No2):
+         print("Maximum is : ",No1)
+    else:
+         print("Maximum is : ",No2)
+
+
+ 
+main()
