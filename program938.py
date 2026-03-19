654e80112628e7cc522ea0e64e24907a4d186ac1
Aditya Shejwal <adityashejwal016@gmail.com>
2026-03-19 17:58:07 +0530

    Add files via upload

---
+++ program938.py
@@ -0,0 +1,13 @@
+# Generic
+
+def Addition(A,B):
+    Result = 0
+    Result = A+B
+    return Result
+
+def main():
+    print(Addition(10,21))
+    print(Addition(10.3,11.2))
+    print(Addition("Hello","World"))
+ 
+main()
