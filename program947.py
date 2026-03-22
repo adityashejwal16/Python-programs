47c25df1d5dac30da704d1314873ac16df1f024f
Aditya Shejwal <adityashejwal016@gmail.com>
2026-03-22 23:00:51 +0530

    Add files via upload

---
+++ program947.py
@@ -0,0 +1,17 @@
+# Factors
+
+def DisplayFactors(No):
+    for i in range(1, int(No/2)+1):
+        if(No % i == 0):
+            print(i)
+
+
+def main():
+    Value = 0
+
+    print("Enter Number : ")
+    Value = int(input())
+
+    DisplayFactors(Value)
+
+main()
