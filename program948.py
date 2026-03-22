6cbf9c7df422097b46e10dbb516d5a7d20af63c5
Aditya Shejwal <adityashejwal016@gmail.com>
2026-03-22 23:01:11 +0530

    Add files via upload

---
+++ program948.py
@@ -0,0 +1,24 @@
+# Sum Factors
+
+def SumFactors(No):
+
+    iSum = 0
+
+    for i in range(1, int(No/2)+1):
+        if(No % i == 0):
+            iSum = iSum + i
+
+    return iSum        
+
+
+def main():
+    Value = 0
+
+    print("Enter Number : ")
+    Value = int(input())
+
+    Ret = SumFactors(Value)
+
+    print("Summation is Factors :",Ret)
+
+main()
