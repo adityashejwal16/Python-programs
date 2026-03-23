396318d649c848919c1d6c53a79cb98c7a4e5bea
Aditya Shejwal <adityashejwal016@gmail.com>
2026-03-23 17:51:12 +0530

    Add files via upload

---
+++ program959.py
@@ -0,0 +1,17 @@
+# List[]
+# Same As Array
+
+
+
+def main():
+    Size = 0
+    Arr = []
+
+    Arr.append(11)
+    Arr.append(21)
+    Arr.append(51)
+    Arr.append(101)
+
+    print(Arr)
+
+main()
