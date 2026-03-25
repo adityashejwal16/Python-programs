26ae1c35531debb910aa17be8f7d8ead2b4995c6
Aditya Shejwal <adityashejwal016@gmail.com>
2026-03-25 17:39:58 +0530

    Add files via upload

---
+++ program964.py
@@ -0,0 +1,14 @@
+# char List[]
+# Same As Character Array
+
+   
+
+def main():
+   print("Enter String  : ")
+   Arr = input()
+
+   print("Entered String is :",Arr)
+
+   print("Length of Sting is : ",len(Arr))
+
+main()
