25cdee57450d9a0f1f0b2d063f62f0abb84b49e6
Aditya Shejwal <adityashejwal016@gmail.com>
2026-03-29 13:20:50 +0530

    Add files via upload

---
+++ program975.py
@@ -0,0 +1,28 @@
+
+# HELLO   -> hello
+
+
+def LowerCase(Brr):
+    Result = ""
+
+    for ch in Brr:
+       if(ch >= 'A' and ch <= 'Z'):
+          Result = Result + chr(ord(ch)+ 32)
+
+       elif(ch >= 'a' and ch <= 'z'):
+          Result = Result + chr(ord(ch) - 32)
+          
+       else:
+         Result = Result + ch
+
+    return Result      
+
+def main():
+   print("Enter String  : ")
+   Arr = input()
+
+   Ret = LowerCase(Arr)
+
+   print("Updated String is :",Ret)
+
+main()
