da1e8f1fea7d00cfecd7f86e4435830a22ce0c56
Aditya Shejwal <adityashejwal016@gmail.com>
2026-04-08 17:32:31 +0530

    Add files via upload

---
+++ program981.py
@@ -0,0 +1,15 @@
+# Pattern for All Aplicstion
+
+# Change as per the problem statement
+def Addition(A, B):
+    return A + B
+
+# Fixed
+def main():
+    print("Inside main Function")
+    Ret = Addition(10,11)
+    print("Addition is :",Ret)
+
+# Fixed
+if __name__=="__main__":
+     main()
