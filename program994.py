4072930d41e1fec6619be917e23857bbd010e0de
Aditya Shejwal <adityashejwal016@gmail.com>
2026-04-12 18:49:10 +0530

    Add files via upload

---
+++ program994.py
@@ -0,0 +1,16 @@
+print("Enter the size of array")
+size = int(input())
+
+Arr = [0] * size
+
+print("Size of Array is : ",len(Arr))
+
+print(Arr[0])
+print(Arr[1])
+print(Arr[2])
+print(Arr[3])
+print(Arr[4])
+
+# free(Arr)
+# delete Arr
+del Arr
